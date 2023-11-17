be854b8c86ee355b1c2387e7872baa15bf73e9fb
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2023-11-17 18:56:24 +0400

    Changed auth component

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 import { NO_INDEX_PAGE } from '@/ts/seo.constants'
-import { Auth } from '@/components/Auth'
+import { Auth } from '@/components/Auth';
 
 export const metadata: Metadata = {
 	title: 'Register',
---
+++ frontend/src/components/Auth/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { IAuth } from '@/ts/type'
+import Field from '@/ui/Field'
+import { AtSign, Lock } from 'lucide-react'
+import { Button } from "@/ui/Button";
+import styles from './Auth.module.scss'
+
+export function Auth({ type }: IAuth) {
+	return (
+		<div className={styles.root}>
+			<form>
+				<h1 className={''}>{type}</h1>
+        <div className={styles.fields}>
+          <Field placeholder={'Email'} type={'email'} Icon={AtSign} />
+          <Field placeholder={'Password'} type={'password'} Icon={Lock} />
+        </div>
+        <div className={styles.buttons}>
+          <Button isLoading={false}>{type}</Button>
+        </div>
+			</form>
+		</div>
+	)
+}
